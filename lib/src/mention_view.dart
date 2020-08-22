80972dfd06657e7282fd5c21dc8183dede76cd58
Hafeez Ahmed <imhafeez.ahmed@gmail.com>
2020-08-22 12:27:05 +0500

    add pre-filled value to the FlutterMentions (#3)
    * add prefilled text
    
    add text to the FlutterMentions widget
    
    * add type to text
    
    * Update README.md
    
    * change text variable to defaultText

--- lib/src/mention_view.dart
+++ lib/src/mention_view.dart
@@ -4,6 +4,7 @@ class FlutterMentions extends StatefulWidget {
   FlutterMentions({
     @required this.mentions,
     Key key,
+    this.defaultText,
     this.suggestionPosition = SuggestionPosition.Bottom,
     this.suggestionListHeight = 300.0,
     this.onMarkupChanged,
@@ -45,6 +46,9 @@ class FlutterMentions extends StatefulWidget {
     this.autofillHints,
     this.appendSpaceOnAdd = true,
   }) : super(key: key);
+
+  /// text
+  final String defaultText;
 
   /// List of Mention that the user is allowed to triggered
   final List<Mention> mentions;
@@ -265,6 +269,8 @@ class FlutterMentionsState extends State<FlutterMentions> {
 
     controller = AnnotationEditingController(data);
 
+    controller.text = widget.defaultText;
+    
     // setup a listener to figure out which suggestions to show based on the trigger
     controller.addListener(() {
       final cursorPos = controller.selection.baseOffset;
