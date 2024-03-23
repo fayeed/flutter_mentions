2ca30ff5e2e460065071e417fecffbe3993d9c0d
maRci002 <matuzmarci@gmail.com>
2024-03-23 11:59:44 +0100

    make maxLines nullable

--- lib/src/mention_view.dart
+++ lib/src/mention_view.dart
@@ -142,7 +142,7 @@ class FlutterMentions extends StatefulWidget {
   final bool enableSuggestions;
 
   /// {@macro flutter.widgets.editableText.maxLines}
-  final int maxLines;
+  final int? maxLines;
 
   /// {@macro flutter.widgets.editableText.minLines}
   final int? minLines;
